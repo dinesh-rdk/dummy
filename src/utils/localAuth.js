30d60c23ac2b74a7f0e519d1b10c7c138d83bd0a
nichesoft34 <rahuldinesh1991@gmail.com>
2019-09-26 17:55:40 +0530

    Remove hardcoding & use env values

--- src/utils/localAuth.js
+++ src/utils/localAuth.js
@@ -1,12 +1,12 @@
-const TOKEN_KEY = 'jwt';
+const TOKEN = process.env.REACT_APP_LOCAL_STORAGE_TOKEN_NAME;
 
 export const setToken = token => {
-  localStorage.setItem(TOKEN_KEY, token);
+  localStorage.setItem(TOKEN, token);
 };
 
 export const removeToken = () => {
-  localStorage.removeItem(TOKEN_KEY);
+  localStorage.removeItem(TOKEN);
 };
 
 export const isTokenPresent = () =>
-  localStorage.getItem(TOKEN_KEY) ? true : false;
+  localStorage.getItem(TOKEN) ? true : false;
